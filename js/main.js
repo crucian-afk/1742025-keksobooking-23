3bf8b522acaf302da9684ae78530e05d0c9463b8
Юрий <y.headman@ya.ru>
2021-05-29 18:21:53 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 // На основе материала с JAVASCRIPT.RU https://learn.javascript.ru/task/random-int-min-max
-function randomInt(min, max) {
-  const num = min + Math.random() * (max + 1 - min);
-  return Math.floor(num);
-}
-randomInt(2, 9);
-
-// На основе материала с MDN о методе toFixed()
-function randomFloat(min, max, symbolsAfterComma) {
-  let num = min + Math.random() * (max - min);
-  (min > max) ? num = null : num = num.toFixed(symbolsAfterComma);
+function getRandomInt(min, max) {
+  let num;
+  if (max > min && min >= 0) {
+    num = Math.floor( min + Math.random() * (max + 1 - min) );
+  } else { num = null; }
   return num;
 }
-randomFloat(2, 9, 6);
+getRandomInt(0, 0);
+
+// На основе материала с MDN о методе toFixed()
+function getRandomFloat(min, max, symbolsAfterComma) {
+  let num = min + Math.random() * (max - min);
+  if (min < max && min >= 0) {
+    num = num.toFixed(symbolsAfterComma);
+  } else {
+    num = null;
+  }
+  return num;
+}
+
+getRandomFloat(10, 8, 3);
