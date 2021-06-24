2639bb0597caeb3187e054e0ec0636773479ec59
Юрий <y.headman@ya.ru>
2021-06-25 00:15:42 +0300

    Отрисуй меня полностью (часть 1)

--- js/show-card.js
+++ js/show-card.js
@@ -14,35 +14,6 @@ const popupPhotos = card.querySelector('.popup__photos');
 const popupPhoto = card.querySelector('.popup__photo');
 const popupAvatar = card.querySelector('.popup__avatar');
 
-popupTitle.textContent = advertise.offer.title;
-popupAddress.textContent = advertise.offer.address;
-popupPrice.textContent = `${String(advertise.offer.price)} ₽/ночь`;
-popupCapacity.textContent = `${advertise.offer.rooms} комнаты для ${advertise.offer.guests} гостей`;
-popupTime.textContent = `Заезд после ${advertise.offer.checkin}, выезд до ${advertise.offer.checkout}`;
-popupAvatar.src = advertise.author.avatar;
-
-if (popupDescription) {
-  popupDescription.textContent = advertise.offer.description;
-} else {
-  popupDescription.remove();
-}
-
-popupPhoto.remove();
-
-const photosFragment = document.createDocumentFragment();
-
-for (let i = 0; i < advertise.offer.photos.length; i++) {
-  const photo = document.createElement('img');
-  photo.src = advertise.offer.photos[i];
-  photo.classList.add('popup__photo');
-  photo.width = 45;
-  photo.height = 40;
-  photo.alt = 'Фотография жилья';
-  photosFragment.appendChild(photo);
-}
-
-popupPhotos.appendChild(photosFragment);
-
 const houseType = {
   flat: 'Квартира',
   bungalow: 'Бунгало',
@@ -51,21 +22,54 @@ const houseType = {
   hotel: 'Отель',
 };
 
-popupType.textContent = houseType[advertise.offer.type];
+const generateCard = (ad) => {
+  popupTitle.textContent = ad.offer.title;
+  popupAddress.textContent = ad.offer.address;
+  popupPrice.textContent = `${String(ad.offer.price)} ₽/ночь`;
+  popupCapacity.textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  popupTime.textContent = `Заезд после ${ad.offer.checkin}, выезд до ${ad.offer.checkout}`;
+  popupAvatar.src = ad.author.avatar;
 
-for (const feat of popupFeatures) {
-  let hasFeature = false;
-  for (let i = 0; i < popupFeatures.length; i++) {
-    if (feat.classList.contains(`popup__feature--${advertise.offer.features[i]}`)) {
-      hasFeature = true;
+  if (popupDescription) {
+    popupDescription.textContent = ad.offer.description;
+  } else {
+    popupDescription.remove();
+  }
+
+  popupPhoto.remove();
+
+  const photosFragment = document.createDocumentFragment();
+
+  for (let i = 0; i < ad.offer.photos.length; i++) {
+    const photo = document.createElement('img');
+    photo.src = ad.offer.photos[i];
+    photo.classList.add('popup__photo');
+    photo.width = 45;
+    photo.height = 40;
+    photo.alt = 'Фотография жилья';
+    photosFragment.appendChild(photo);
+  }
+
+  popupPhotos.appendChild(photosFragment);
+
+  popupType.textContent = houseType[ad.offer.type];
+
+  for (const feat of popupFeatures) {
+    let hasFeature = false;
+    for (let i = 0; i < popupFeatures.length; i++) {
+      if (feat.classList.contains(`popup__feature--${ad.offer.features[i]}`)) {
+        hasFeature = true;
+      }
+
     }
-
+    if (!hasFeature) {
+      feat.remove();
+    }
   }
-  if (!hasFeature) {
-    feat.remove();
-  }
-}
 
-const adField = document.querySelector('#map-canvas');
+  const adField = document.querySelector('#map-canvas');
 
-adField.appendChild(card.cloneNode(true));
+  adField.appendChild(card.cloneNode(true));
+};
+
+generateCard(advertise);
